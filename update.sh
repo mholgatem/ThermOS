9b0385cfb4cba815fcc073f8674f7a500fa533c4
mholgatem <holgate.mark1@gmail.com>
2017-09-27 17:24:09 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,42 +1,12 @@
-set -o errexit
+VERSION="1.0"
 
-VERSION="1.1"
-SELF=$(basename $0)
-UPDATE_BASE=https://raw.githubusercontent.com/mholgatem/ThermOS/master
+shopt -s nocasematch
 
-runSelfUpdate() {
-  echo "Performing self-update..."
-
-  # Download new version
-  echo -n "Downloading latest version of updater..."
-  if ! wget --quiet --output-document="$0.tmp" $UPDATE_BASE/$SELF ; then
-    echo "Failed: Error while trying to wget new version!"
-    echo "File requested: $UPDATE_BASE/$SELF"
-    exit 1
-  fi
-  echo "Done."
-
-  # Copy over modes from old version
-  OCTAL_MODE=$(stat -c '%a' $SELF)
-  if ! chmod $OCTAL_MODE "$0.tmp" ; then
-    echo "Failed: Error while trying to set mode on $0.tmp."
-    exit 1
-  fi
-
-  # Spawn update script
-  cat > updateScript.sh << EOF
-#!/bin/bash
-# Overwrite old file with new
-if mv "$0.tmp" "$0"; then
-  echo "Done. Update complete."
-  rm \$0
-else
-  echo "Failed!"
+echo "Performing self-update..."
+if ! [[ "$1" == "-noupdate" ]]; then
+    git fetch --all
+    git reset --hard origin/master
+    exec /bin/bash update.sh -noupdate
 fi
-EOF
 
-  echo -n "Inserting update process..."
-  exec /bin/bash updateScript.sh
-}
-
-runSelfUpdate
+echo 'complete'
