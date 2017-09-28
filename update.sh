93935eb4b59ae695c9529008ed9452dc5fd7a8ae
mholgatem <holgate.mark1@gmail.com>
2017-09-27 22:57:36 -0700

    updater test

--- update.sh
+++ update.sh
@@ -4,8 +4,14 @@ shopt -s nocasematch
 
 echo "Performing self-update..."
 if ! [[ "$1" == "-noupdate" ]]; then
-    git fetch --all
-    git reset --hard origin/master
+    git config --global user.email "none@none.com"
+    git config --global user.name "none@none.com"
+    git checkout master
+    git stash
+    git pull
+    git stash pop
+    git config --global --unset user.email
+    git config --global --unset user.name
     exec /bin/bash update.sh -noupdate
 fi
 
